5d9050a24c302b033ae3585a4b45f814508e1c30
Edmundo Pichardo <ce.pichardo@gmail.com>
2016-02-20 12:17:38 -0400

    Add Create, update , destroy the post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -20,15 +23,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
   def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+        render "eit"
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
 
   def find_post
+    @post = Post.find(params[:id])
   end
 
   def post_params
